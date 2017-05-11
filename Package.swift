495d2f0b6140f6d40b10177ce578503bb3fdd1f2
Chris <ccorn90@gmail.com>
2017-05-11 14:12:12 -0400

    Use Swift-Language-Mods for eachWithObject

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "shed",
     dependencies: [
-        .Package(url: "https://github.com/ccorn90/Spec.git", majorVersion: 0)
+        .Package(url: "https://github.com/ccorn90/Spec.git", majorVersion: 0),
+        .Package(url: "https://github.com/ccorn90/Swift-Language-Mods.git", majorVersion: 1)
     ]
 )
