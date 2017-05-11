0f857748a476105ce094d5a57d7528e41be89411
Chris <ccorn90@gmail.com>
2017-05-11 12:34:11 -0400

    Move around

--- Sources/Card.swift
+++ Sources/Card.swift
@@ -13,21 +13,3 @@ public enum Card : UInt {
     case king = 13
     case ace = 14
 }
-
-// implement supercession logic for the Shed game:
-public func valid(play: Card, onCard: Card) -> Bool {
-    // two can always be played:
-    if play == .two { return true }
-
-    switch onCard {
-        // everything can be played on a two:
-        case .two: return true
-
-        // nine only allows less-than or equal to:
-        case .nine: return play.rawValue <= Card.nine.rawValue
-
-        // compare based on the raw value:
-        default: return play.rawValue >= onCard.rawValue
-    }
-}
-
--- Sources/Plays.swift
+++ Sources/Plays.swift
@@ -1,5 +1,22 @@
+// Supercession logic for the Shed game:
+public func valid(play: Card, onCard: Card) -> Bool {
+    // two can always be played:
+    if play == .two { return true }
+
+    switch onCard {
+        // everything can be played on a two:
+        case .two: return true
+
+        // nine only allows less-than or equal to:
+        case .nine: return play.rawValue <= Card.nine.rawValue
+
+        // compare based on the raw value:
+        default: return play.rawValue >= onCard.rawValue
+    }
+}
+
 // Return all possible plays given hand and pile.  Top of pile is at the head of list.
-func plays(forHand hand: [Card], onPile pile: [Card]) -> [[Card]] {
+public func plays(forHand hand: [Card], onPile pile: [Card]) -> [[Card]] {
     let topCard = pile.first
     let playableCards = hand.filter({ topCard == nil || valid(play: $0, onCard: topCard!) })
     let countedPlayableCards: [Card : Int] = playableCards.eachWithObject([:]) { hash, card in
